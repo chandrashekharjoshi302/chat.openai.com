b254897a4e9739d3ec9ece2f412e69358efe9f19
chandrashekharjoshi302 <chandrashekharjoshi02@gmail.com>
2023-05-05 11:31:23 +0530

    chat.openai.com

---
+++ server/index.js
@@ -0,0 +1,52 @@
+const OpenAI = require("openai");
+const { Configuration, OpenAIApi } = OpenAI;
+
+const express = require("express");
+const bodyParser = require("body-parser");
+const cors = require('cors');
+const app = express();
+
+require("dotenv").config();
+
+const configuration = new Configuration({
+    apiKey: process.env.SECRET_KEY,
+});
+
+const openai = new OpenAIApi(configuration);
+
+app.use(express.json());
+app.use(bodyParser.json());
+app.use(cors());
+
+app.get("/a", (req, res) => {
+    res.send("hello");
+});
+
+app.post("/", async (req, res) => {
+    try {
+        const { message } = req.body;
+       
+        if (!message) {
+            throw new Error("Missing input parameter: message");
+        }
+        const response = await openai.createCompletion({
+            model: "text-davinci-003",
+            prompt: `${message}`,
+            maxTokens: 100,
+            temperature: 0,
+        });
+       
+        if (response.data.choices[0].text) {
+            res.json({
+                message: response.data.choices[0].text,
+            });
+        }
+    } catch (error) {
+        console.log(error);
+        
+    }
+});
+
+const port = process.env.PORT ;
+
+app.listen(port, () => console.log(`Server listening on port ${port}`));
